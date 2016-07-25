abe60113433ab0c47c0f302d8ae6d29fca6d884d
Davide Madrisan <davide.madrisan@gmail.com>
2016-07-25 14:18:11 +0200

    legraph.py: fix the license notice
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- legraph.py
+++ legraph.py
@@ -5,7 +5,7 @@
 
 __author__ = "Davide Madrisan"
 __copyright__ = "Copyright (C) 2016 Davide Madrisan"
-__license__ = "GPL"
+__license__ = "Apache License 2.0"
 __version__ = "2"
 __email__ = "davide.madrisan.gmail.com"
 __status__ = "stable"
