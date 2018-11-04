f8a5916e8aef85f0843d0999ffc58b48769f93f9
Davide Madrisan <davide.madrisan@gmail.com>
2018-11-05 00:03:11 +0200

    Bump version to 4
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- dynamic-systems-and-chaos/utils.py
+++ dynamic-systems-and-chaos/utils.py
@@ -7,7 +7,7 @@
 __author__ = "Davide Madrisan"
 __copyright__ = "Copyright (C) 2016-2018 Davide Madrisan"
 __license__ = "Apache License 2.0 (Apache-2.0)"
-__version__ = "3"
+__version__ = "4"
 __email__ = "davide.madrisan@gmail.com"
 __status__ = "stable"
 
